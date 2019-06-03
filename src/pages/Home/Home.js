1b7a1403fdfe32d316b872460c9a07a7544d79e8
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-06-03 12:20:37 -0700

    landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
 import Layout from "../../components/Layout/Layout";
-import introImg from "../../assets/images/home/camion.png";
 import "./Home.css";
+
+import Link from "react-router-dom/Link";
+import ReactPlayer from "react-player";
 
 const transparent = "#00000000";
 const mainColor = "#2D3B97";
@@ -41,18 +43,39 @@ export class HomePage extends Component {
     return (
       <Layout navbarColor={this.state.navbarColor} mainMargin={0}>
         <div className="home">
-          <img src={introImg} className="img-fluid" alt="autotransporte"></img>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
-          <p>Landing page</p>
+          <section className="intro-video">
+            <div className='player-wrapper'>
+              <ReactPlayer className="react-player"
+                url='https://www.youtube.com/watch?v=6C0fVCahkhQ'
+                playing loop muted controls={false}
+                width='100%' height='100%'
+                config={{
+                  youtube: {
+                    playerVars: { showinfo: 0 }
+                  }
+                }} />
+            </div>
+          </section>
+
+          <section className="about">
+            <div className="about-info">
+              <h2 className="subtitle">Nuestra empresa está <span className="text-red">100%</span> dedicada al transporte y logística</h2>
+              <p>Legalmente establecida en México bajo las leyes y reglamentos de la SCT. Somos una compañía certificada por la norma C-TPAT. Somos una empresa extremadamente eficiente e innovadora que estamos en constante crecimiento y desarrollo con más de 10 años de experiencia en el sector.</p>
+            </div>
+          </section>
+
+          <section className="map">
+            <h2 className="subtitle">Facilidad y seguridad en los traslados en México y a USA</h2>
+          </section>
+
+          <section className="quotation">
+            <div className="quotation-info">
+              <h2 className="subtitle">Acércate a nosotros</h2>
+              <Link to="/cotiza">
+                <button className="btn btn-sec btn-big">Cotiza ahora</button>
+              </Link>
+            </div>
+          </section>
 
         </div>
       </Layout >
